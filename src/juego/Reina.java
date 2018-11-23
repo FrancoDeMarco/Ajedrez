e4864a01a431c17bca06f7b1bbd88b62b20fabe7
Emiliano De Marco Andrada <emilianodemarcoandrada@gmail.com>
2018-11-23 16:49:17 -0300

    reina lista

--- src/juego/Reina.java
+++ src/juego/Reina.java
@@ -33,7 +33,105 @@ public class Reina extends Pieza{
 		}
 	}*/
 
-	public boolean movimientoValido(Celda nuevaCelda) {//EL MOVIMIENTO DE LA REINA ES UNA COMBINACION ENTRE EL ALFIL Y LA TORRE
+ 	public boolean movimientoValido(Celda nuevaCelda) {//EL MOVIMIENTO DE LA REINA ES UNA COMBINACION ENTRE EL ALFIL Y LA TORRE
+		int filaN = nuevaCelda.getFila();
+		int columnaN = nuevaCelda.getColumna();
+		int filaV = this.getCelda().getFila();
+		int columnaV = this.getCelda().getColumna();
+		
+		//SI SE VA A MOVER COMO ALFIL
+		if(((this.getCelda().getColumna()-this.getCelda().getFila()) == (nuevaCelda.getColumna()-nuevaCelda.getFila()))||
+		((this.getCelda().getColumna()+this.getCelda().getFila()) == (nuevaCelda.getColumna()+nuevaCelda.getFila()))) {//CONDICION DEL MOVIMIENTO DEL ALFIL
+	        if (!nuevaCelda.estaOcupadaPorElMismoEquipo(this.getEquipo())) {
+				int i = 0;
+				int j = 0;
+				if (columnaV < columnaN) { // -> +
+					if (filaV < filaN) { // v +
+						for (i = 1;  filaV+i < filaN;  i++) { //Recorro el camino, si esta ocupado por otra pieza (de cualquier equipo) retorna falso
+							j++;
+							if(this.getEquipo().getAjedrez().getTablero().getCelda(filaV+i, columnaV+j).getPieza() != null) {
+									return false;
+							}
+						}
+					} else { // ^ -
+						for (i = -1;  filaV+i > filaN;  i--) { //Recorro el camino, si esta ocupado por otra pieza (de cualquier equipo) retorna falso
+							j++;
+							if(this.getEquipo().getAjedrez().getTablero().getCelda(filaV+i, columnaV+j).getPieza() != null) {
+								return false;
+							}
+						}
+					}
+				} else { // <- -
+					if (filaV < filaN) { // v +
+						for (i = 1;  filaV+i < filaN;  i++) { //Recorro el camino, si esta ocupado por otra pieza (de cualquier equipo) retorna falso
+							j--;
+							if(this.getEquipo().getAjedrez().getTablero().getCelda(filaV+i, columnaV+j).getPieza() != null) {
+									return false;
+							}
+						}					
+					} else { // ^ -
+						for (i = -1;  filaV+i > filaN;  i--) { //Recorro el camino, si esta ocupado por otra pieza (de cualquier equipo) retorna falso
+							j--;
+
+							if(this.getEquipo().getAjedrez().getTablero().getCelda(filaV+i, columnaV+j).getPieza() != null) {
+									return false;
+							}
+						}
+					}
+				}
+				return true;
+	        }else {
+	        	return false;
+	        }
+		}else { //SI SE VA A MOVER COMO TORRE
+			if (((this.getCelda().getFila()==nuevaCelda.getFila()))||(this.getCelda().getColumna()==nuevaCelda.getColumna())){
+				if (!nuevaCelda.estaOcupadaPorElMismoEquipo(this.getEquipo())) {
+					if (columnaV == columnaN) { //^v
+						if (filaV > filaN) { //^
+							for (int i = 1; i < Math.abs(filaV-filaN); i++) {
+								if(this.getEquipo().getAjedrez().getTablero().getCelda(filaV-i, columnaV).getPieza() != null) {
+									return false;
+								}
+							}
+						} else {//v
+							for (int i = 1; i < Math.abs(filaV-filaN); i++) {
+								if(this.getEquipo().getAjedrez().getTablero().getCelda(filaV+i, columnaV).getPieza() != null) {
+									return false;
+								}
+							}
+						}
+					} else { //<--->
+						if (columnaV > columnaN) { // <
+							for (int i = 1; i < Math.abs(columnaV-columnaN); i++) {
+								if(this.getEquipo().getAjedrez().getTablero().getCelda(filaV, columnaV-i).getPieza() != null) {
+									return false;
+								}
+							}
+						}else { // >
+							for (int i = 1; i < Math.abs(columnaV-columnaN); i++) {
+								if(this.getEquipo().getAjedrez().getTablero().getCelda(filaV, columnaV+i).getPieza() != null) {
+									return false;
+								}
+							}
+						}
+					}
+					
+					
+					
+					return true;
+				}else {
+					return false;
+				}
+			}else {
+				return  false;
+			}
+		}
+	}
+	
+	
+/*
+ 	public boolean movimientoValido(Celda nuevaCelda) {//EL MOVIMIENTO DE LA REINA ES UNA COMBINACION ENTRE EL ALFIL Y LA TORRE
+ 
 		if((this.getCelda().getFila()==nuevaCelda.getFila()||(this.getCelda().getColumna() == nuevaCelda.getColumna())) 
 		||((this.getCelda().getFila()-nuevaCelda.getFila())==(this.getCelda().getColumna() - nuevaCelda.getColumna())
 		||(this.getCelda().getFila()+nuevaCelda.getFila())==(this.getCelda().getColumna()+nuevaCelda.getColumna()))
@@ -44,7 +142,7 @@ public class Reina extends Pieza{
 		}
 		
 	}
-	
+*/	
 	/*@Override
 	public String toString() {
 		return "D" + super.toString();
